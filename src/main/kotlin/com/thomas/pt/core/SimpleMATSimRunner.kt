ff1da8995b52ea59811c166000d192245408c7dc
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2026-01-29 12:49:21 +0700

    feat: add simple MATSim runner

--- src/main/kotlin/com/thomas/pt/core/SimpleMATSimRunner.kt
+++ src/main/kotlin/com/thomas/pt/core/SimpleMATSimRunner.kt
@@ -1,15 +1,12 @@
-package com.thomas.pt.utils
+package com.thomas.pt.core
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.path
-import com.thomas.pt.core.RunMatsim
 
-@Suppress("unused")
-class SimpleMATSimRunner: CliktCommand() {
+object SimpleMATSimRunner: CliktCommand(name = "simple-run") {
     val config by option("--matsim-cfg").path(
         mustExist = true,
         mustBeReadable = true,
@@ -18,5 +15,3 @@ class SimpleMATSimRunner: CliktCommand() {
 
     override fun run() = RunMatsim.simpleRun(config)
 }
-
-//fun main(args: Array<String>) = SimpleMATSimRunner().main(args)
