7c3f0af97eee67caeaa0bc61e770f5e69d3ccf9f
Minh Hai Hoang <122735724+Thomas-Hoang-04@users.noreply.github.com>
2026-02-11 17:22:25 +0700

    doc: update project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
     id("com.gradle.develocity") version "4.2.2"
 }
-rootProject.name = "Bus_MATSim"
+rootProject.name = "MATSim_Bus_Optimizer"
